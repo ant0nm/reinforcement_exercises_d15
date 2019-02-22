d2e58b797aacd71bbe5906ccffcd17ed48fb7e17
Anton <anton.mois33v@gmail.com>
2019-02-22 12:16:32 -0500

    Completed exercise 1

---
+++ exercises.py
@@ -0,0 +1,10 @@
+# exercise 1
+class Task:
+
+    def __init__(self, description, due_date):
+        self.description = description
+        self.due_date = due_date
+
+task1 = Task("get groceries", "today")
+task2 = Task("wash the car", "tomorrow")
+task3 = Task("do laundry", "today")
